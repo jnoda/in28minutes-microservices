ebc0b717a24cb4287a11b3a00cfdf6b98630c3cb
Julio Noda <julio.noda@gmail.com>
2021-04-06 00:07:30 +0200

    Using the naming server to register and discover services
    ...an added bonus is the automated load balancing of requests by Feign clients.

--- currency-conversion-service/src/main/java/com/julionoda/in28minutes/microservices/currencyconversionservice/CurrencyExchangeService.java
+++ currency-conversion-service/src/main/java/com/julionoda/in28minutes/microservices/currencyconversionservice/CurrencyExchangeService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000", decode404 = true)
+@FeignClient(name = "currency-exchange-service", decode404 = true)
 public interface CurrencyExchangeService {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     Optional<CurrencyConversion> retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
